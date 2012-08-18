9ed082033014dcd6297cd51ba6bd2207ff8d7d89
k <admin@k23k.in>
2012-08-18 11:05:56 +0530

    Indentation patch

--- server.rb
+++ server.rb
@@ -6,34 +6,32 @@ welcome = "Welcome! type EXIT to quit\000"
 byebye	= "Disconnected from server\000"
 eof     = '\000'
 
-url			= "127.0.0.1"
-port		= 9999
+url  = "127.0.0.1"
+port = 9999
 server	= TCPServer.new(url, port)
 puts init 
 
 while (socket = server.accept)
-	socket.puts welcome 
+    socket.puts welcome 
 
-	quit = false
-	while !quit
-		msg		= socket.gets
-		if msg.nil?
-			quit = true
-		else
-			msg		= msg[1..-1] if msg[0] == "\000"
-		end
+    quit = false
+    while !quit
+        msg		= socket.gets
+            if msg.nil?
+                quit = true
+            else
+                msg = msg[1..-1] if msg[0] == "\000"
+            end
 
-		if msg.chomp != "EXIT"
-			socket.puts "<b>me</b>: #{msg.chomp}\000"
-		else
-			quit = true
-		end
-	end
+            if msg.chomp != "EXIT"
+                socket.puts "<b>me</b>: #{msg.chomp}\000"
+            else
+                quit = true
+            end
+    end
 
-	if quit
-		socket.puts byebye
-#		socket.puts eof
-		socket.close
-	end
-
+    if quit
+        socket.puts byebye
+        socket.close
+    end
 end
--- simpleServer.java
+++ simpleServer.java
@@ -4,51 +4,51 @@ import java.net.*;
 
 public class simpleServer
 {
-	public static void main(String args[])
-	{
-		// Message terminator
-		char EOF = (char)0x00;
-		
-		try
-		{
-			// create a serverSocket connection on port 9999
-			ServerSocket s = new ServerSocket(9999);
-			
-			System.out.println("Server started. Waiting for connections...");
-			// wait for incoming connections
-			Socket incoming = s.accept();
+    public static void main(String args[])
+    {
+        // Message terminator
+        char EOF = (char)0x00;
 
-			BufferedReader data_in = new BufferedReader(
-					new InputStreamReader(incoming.getInputStream()));
-			PrintWriter data_out = new PrintWriter(incoming.getOutputStream());
-			
-			data_out.println("Welcome! type EXIT to quit." + EOF);
-			data_out.flush();
-		
-			boolean quit = false;
-			
-			// Waits for the EXIT command
-			while (!quit)
-			{
-				String msg = data_in.readLine();
-	
-				if (msg == null) quit = true;
-				
-				if (!msg.trim().equals("EXIT"))
-				{
-					data_out.println("You said: <b>"+msg.trim()+"</b>"+EOF);
-					data_out.flush();
-				}
-				else
-				{
-					quit = true;
-				}
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-			System.out.println("Connection lost");
-		}
-	}
+        try
+        {
+            // create a serverSocket connection on port 9999
+            ServerSocket s = new ServerSocket(9999);
+
+            System.out.println("Server started. Waiting for connections...");
+            // wait for incoming connections
+            Socket incoming = s.accept();
+
+            BufferedReader data_in = new BufferedReader(
+                new InputStreamReader(incoming.getInputStream()));
+            PrintWriter data_out = new PrintWriter(incoming.getOutputStream());
+
+            data_out.println("Welcome! type EXIT to quit." + EOF);
+            data_out.flush();
+
+            boolean quit = false;
+
+            // Waits for the EXIT command
+            while (!quit)
+            {
+                String msg = data_in.readLine();
+
+                if (msg == null) quit = true;
+
+                if (!msg.trim().equals("EXIT"))
+                {
+                    data_out.println("You said: <b>"+msg.trim()+"</b>"+EOF);
+                    data_out.flush();
+                }
+                else
+                {
+                    quit = true;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Connection lost");
+        }
+    }
 }
